913360fec8c1b16ddaac404f79df825024c98dd3
Ashley Williams <ashley666ashley@gmail.com>
2016-03-08 22:41:06 -0500

    remove last sudo from no-sudo script

--- deploy-nosudo.sh
+++ deploy-nosudo.sh
@@ -1,7 +1,7 @@
 apt-get update
 apt-get install -y g++
 apt-get install -y curl
-curl -sL https://deb.nodesource.com/setup_4.x | sudo sh
+curl -sL https://deb.nodesource.com/setup_4.x | sh
 apt-get install -y nodejs 
 npm i -g npm@latest
 node -v && npm -v
