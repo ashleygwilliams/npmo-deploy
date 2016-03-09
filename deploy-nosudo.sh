280b1448db0a2b477b6ea25ece7c29d4ef3ae036
Ashley Williams <ashley666ashley@gmail.com>
2016-03-08 22:34:23 -0500

    add no-sudo dpeloy script

---
+++ deploy-nosudo.sh
@@ -0,0 +1,8 @@
+apt-get update
+apt-get install -y g++
+apt-get install -y curl
+curl -sL https://deb.nodesource.com/setup_4.x | sudo sh
+apt-get install -y nodejs 
+npm i -g npm@latest
+node -v && npm -v
+npm install npmo -g --unsafe
